bc7077301ede1932f39d09e083664aaff2c95f4c
geraa1985 <geraa1985@gmail.com>
2021-01-20 16:19:41 +0300

    Some changes

--- app/src/main/java/com/geraa1985/rentateamtest/mvp/model/entities/room/cache/IUsersCache.kt
+++ app/src/main/java/com/geraa1985/rentateamtest/mvp/model/entities/room/cache/IUsersCache.kt
@@ -4,9 +4,6 @@ import com.geraa1985.rentateamtest.mvp.model.entities.base.User
 import io.reactivex.rxjava3.core.Single
 
 interface IUsersCache {
-    fun putUsers(users: List<User>)
     fun getUsers(): Single<List<User>>
     fun putUser(user: User)
-    fun getUser(login: String): Single<User>
-    fun getUsersByName(name: String): Single<List<User>>
 }
--- app/src/main/java/com/geraa1985/rentateamtest/mvp/model/entities/room/dao/IUsersDAO.kt
+++ app/src/main/java/com/geraa1985/rentateamtest/mvp/model/entities/room/dao/IUsersDAO.kt
@@ -9,36 +9,7 @@ interface IUsersDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(user: RoomUser)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(users: List<RoomUser>)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(vararg users: RoomUser)
-
-    @Update
-    fun update(user: RoomUser)
-
-    @Update
-    fun update(users: List<RoomUser>)
-
-    @Update
-    fun update(vararg users: RoomUser)
-
-    @Delete
-    fun delete(user: RoomUser)
-
-    @Delete
-    fun delete(users: List<RoomUser>)
-
-    @Delete
-    fun delete(vararg users: RoomUser)
-
     @Query("SELECT * FROM RoomUser")
     fun getAll(): List<RoomUser>
 
-    @Query("SELECT * FROM RoomUser WHERE firstName = :firstName LIMIT 1")
-    fun getUser(firstName: String): RoomUser?
-
-    @Query("SELECT * FROM RoomUser WHERE firstName LIKE '%' || :firstName  || '%'")
-    fun getUsersByName(firstName: String): List<RoomUser>
 }
